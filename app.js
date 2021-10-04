6c86f30eb2c9f31cb554ea6b3b24c4b8b6a344b8
Paul Pardo <paulpardo@Pauls-MacBook-Pro.local>
2021-10-04 19:15:05 -0400

    Correcting js

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
+
+let food = 'pizza';
+const url = `https://api.spoonacular.com/food/menuItems/search?apiKey=54f523904c8247bda81b6d9f2e9e1322&query=${food}`
+
+console.log(url)
+const foodList = document.getElementById('foods')
+foodList.addEventListener('click', () => {
+  document.querySelector(food).value
+})
+
+fetch(url)
+  .then((res) => { return res.json() })
+  .then((resJSON) => {
+    renderFood(resJSON.menuItems[2]);
+  })
+  
+  
+const renderFood = (resJSON) => {
+  const foodTitle = document.createElement('h4')
+  foodTitle.innerHTML = resJSON.title
+  document.querySelector('#tile-container').append(foodTitle)
+
+  }
+  
+
+/*
 ; (function () {
   let tiles;
   let hasData;
@@ -19,11 +45,13 @@ document.getElementById('tile-container').appendChild(fragment)
   }
 
   async function fetchImgData () {
-  const url = `https://api.spoonacular.com/food/menuItems/search?apiKey=54f523904c8247bda81b6d9f2e9e1322&query=pizza`
+  
   const res = await fetch(url)
   return res.json()
   }
   
+  fetch()
+
   fetchImgData().then((data) => {
     hasData = true;
     tiles = data.menuItems
@@ -41,3 +69,4 @@ document.getElementById('tile-container').appendChild(fragment)
     
 }
 )
+*/
