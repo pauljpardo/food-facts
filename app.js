b2623d8e7dd7163e760f2c8fd480fc00350fb4fe
Paul Pardo <paulpardo@Pauls-MacBook-Pro.local>
2021-10-04 16:00:44 -0400

    Added js

---
+++ app.js
@@ -0,0 +1,47 @@
+; (function () {
+  let tiles;
+  let hasData;
+  let hasLoaded;
+
+  function renderTiles() {
+    const fragment = new DocumentFragment()
+
+    tiles.forEach((tile) => {
+      const div = document.createElement('div')
+      div.innerHTML = `
+      <div class='tile container'>
+      <a href='${tile.url}'> <img src='${tile.image}' class='image'></a>
+      </div> 
+      `
+      fragment.appendChild(div)
+    })
+document.getElementById('tile-container').appendChild(fragment)
+  }
+
+
+
+
+  async function fetchImgData () {
+  const url = `https://api.spoonacular.com/food/menuItems/
+  search?apiKey=54f523904c8247bda81b6d9f2e9e1322&query=pizza`
+  const res = await fetch(url)
+  return res.json()
+  }
+  
+  fetchImgData().then((data) => {
+    hasData = true;
+    tiles = data.menuItems
+    if (hasLoaded) {
+      renderTiles()
+    }
+  })
+
+  window.addEventListener('DOMContentLoaded', (event) => {
+    hasLoaded = true;
+    if (hasLoaded) {
+      renderTiles()
+    }
+  })
+    
+}
+)
