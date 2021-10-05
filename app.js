2c934f8586dcb430e7ea5813ff38e533d2d292fe
Paul Pardo <paulpardo@Pauls-MacBook-Pro.local>
2021-10-05 10:07:25 -0400

    Cleaning up JS

--- app.js
+++ app.js
@@ -1,72 +1,38 @@
 
-let food = 'pizza';
+
+
+const button = document.getElementById('food-button')
+button.addEventListener('click', () => {
+  let allItems = document.querySelectorAll('.item')
+  for (let i = 0; i < allItems.length; i++){
+    allItems[i].remove();
+  }
+  let foodOptions = document.getElementById('allFoods')
+  let food = foodOptions.options[foodOptions.selectedIndex].text
+
 const url = `https://api.spoonacular.com/food/menuItems/search?apiKey=54f523904c8247bda81b6d9f2e9e1322&query=${food}`
 
-console.log(url)
-const foodList = document.getElementById('foods')
-foodList.addEventListener('click', () => {
-  document.querySelector(food).value
-})
 
 fetch(url)
   .then((res) => { return res.json() })
   .then((resJSON) => {
-    renderFood(resJSON.menuItems[2]);
+    const number = (Math.floor(Math.random() * resJSON.menuItems.length))
+    console.log(resJSON)
+    renderFood(resJSON.menuItems[number])
   })
   
   
-const renderFood = (resJSON) => {
+  const renderFood = (resJSON) => {
+  console.log(resJSON)
   const foodTitle = document.createElement('h4')
   foodTitle.innerHTML = resJSON.title
-  document.querySelector('#tile-container').append(foodTitle)
+    document.querySelector('#tile-container').append(foodTitle)
+    foodTitle.className = 'item'
 
+const foodImg = document.createElement('img')
+    foodImg.src = `${ resJSON.image }`
+    document.querySelector('#tile-container').append(foodImg)
+    foodImg.className = 'item'
   }
   
-
-/*
-; (function () {
-  let tiles;
-  let hasData;
-  let hasLoaded;
-
-  function renderTiles() {
-    const fragment = new DocumentFragment()
-
-    tiles.forEach((tile) => {
-      const div = document.createElement('div')
-      div.innerHTML = `
-      <div class='tile-holder'>
-      <a href='${tile.url}'> <img src='${tile.image}' class='image'></a>
-      </div> 
-      `
-      fragment.appendChild(div)
-    })
-document.getElementById('tile-container').appendChild(fragment)
-  }
-
-  async function fetchImgData () {
-  
-  const res = await fetch(url)
-  return res.json()
-  }
-  
-  fetch()
-
-  fetchImgData().then((data) => {
-    hasData = true;
-    tiles = data.menuItems
-    if (hasLoaded) {
-      renderTiles()
-    }
-  })
-
-  window.addEventListener('DOMContentLoaded', (event) => {
-    hasLoaded = true;
-    if (hasData) {
-      renderTiles()
-    }
-  })
-    
-}
-)
-*/
+})
